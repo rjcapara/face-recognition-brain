c768f6308c2bdd6e9f946e4e4c65f7c462eb0d16
RJ <rjcapara14@gmail.com>
2020-04-08 10:43:41 +0800

    deploying

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://radiant-woodland-36769.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
